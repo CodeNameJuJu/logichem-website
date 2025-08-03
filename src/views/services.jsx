37ab5921f8bd901d68b24b3d66c772791e2b50d7
julian Stone <julian@siftan-software.co.za>
2025-08-03 15:47:40 +0200

    Adjusted sizing for images

--- src/views/services.jsx
+++ src/views/services.jsx
@@ -5,12 +5,12 @@ import { servicesData } from '../data/serviceData';
 function Services() {
   return (
     <section id="services">
-      <div className='container pt-3 mt-3 pb-3 mb-3 fadeIn-animation-up' >
+      <div className='container pt-3 mt-3 fadeIn-animation-up' >
         <h3 className="section-heading text-center">Services</h3>
         <div className='row'>
           {servicesData.map((service, index) => (
-            <div key={index} className='col-sm-4 pt-2 mt-2'>
-              <div className='card-container'>
+            <div key={index} className='col-sm-4 pt-2 mt-2 pb-2 mb-2'>
+              <div className='card-container mb-4'>
                 <div className="image-container position-relative">
                   <figure>
                     <img
